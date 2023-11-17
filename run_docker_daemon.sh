44a8c78ce16e61d7785c493202ee53599f973936
Christos Zonios <czonios@gmail.com>
2023-11-17 23:17:38 +0200

    continuously stream logs from docker

--- run_docker_daemon.sh
+++ run_docker_daemon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker run --name task-gopher-server --env-file ./.env --restart unless-stopped -p 8334:8334 -d -t go-containerized:latest
-docker logs task-gopher-server >> ./server_logs.log
+sudo docker logs --follow task-gopher-server >> ./server_logs.log
