06a5acc9489b0d70d84446a5d99dd9b7e382a51c
Christos Zonios <czonios@gmail.com>
2023-11-17 22:53:14 +0200

    improve script

--- run_docker_daemon.sh
+++ run_docker_daemon.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --name task-gopher-server --env-file ./.env --restart unless-stopped -p 8334:8334 go-containerized:latest
+sudo docker run --name task-gopher-server --env-file ./.env --restart unless-stopped -p 8334:8334 go-containerized:latest &> server_logs.log
