54cde53bd669ee532945bdb77b22e2365011afa8
Christos Zonios <czonios@gmail.com>
2023-11-17 23:06:38 +0200

    add -t to container for color and redirect stderr

--- run_docker_daemon.sh
+++ run_docker_daemon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker run --name task-gopher-server --env-file ./.env --restart unless-stopped -p 8334:8334 -d go-containerized:latest
-docker logs task-gopher-server 2>> ./server_logs.log
+sudo docker run --name task-gopher-server --env-file ./.env --restart unless-stopped -p 8334:8334 -d -t go-containerized:latest
+docker logs task-gopher-server >> ./server_logs.log
